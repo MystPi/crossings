0823c29fc66b9be3820158c24edb861571df4848
MystPi <86574651+MystPi@users.noreply.github.com>
2023-09-30 11:56:54 -0400

    🎉 First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Crossings
+
+A tiny library focused on easily connecting JS to HTML.
+
+## Documentation
+
+You can find documentation for Crossings at https://mystpi.github.io/crossings.
---
+++ src/index.js
@@ -0,0 +1,49 @@
+class Bridge {
+  constructor(el = 'body') {
+    this.element = typeof el === 'string' ? document.querySelector(el) : el;
+    this.prevData = {};
+
+    const selected = this.element.querySelectorAll('[data-crossing]');
+
+    this.destinations = Array.from(selected)
+      .map((el) => {
+        const name = el.getAttribute('data-crossing');
+
+        if (!name) {
+          return null;
+        }
+
+        return {
+          name,
+          el,
+        };
+      })
+      .filter(Boolean);
+
+    return new Proxy(this, {
+      set: this.set.bind(this),
+    });
+  }
+
+  cross(data, prop = 'textContent') {
+    this.destinations.forEach((dest) => {
+      const el = dest.el;
+      const name = dest.name;
+      const item = data[name];
+
+      if (item === undefined || this.prevData[name] === item) {
+        return;
+      }
+
+      el[prop] = item;
+    });
+
+    Object.assign(this.prevData, data);
+  }
+
+  set(_, prop, value) {
+    this.cross({
+      [prop]: value,
+    });
+  }
+}
